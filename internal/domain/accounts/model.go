742b41cdd0a989817555cf373bd1984e45a29c5d
Pedro Netto <pedro.nlima@luizalabs.com>
2025-04-03 08:34:19 -0300

    feat: add account constructor

--- internal/domain/accounts/model.go
+++ internal/domain/accounts/model.go
@@ -1,6 +1,14 @@
 package accounts
 
+import "github.com/google/uuid"
+
 type Account struct {
 	Id string `json:"id" bson:"_id"`
+}
+
+func NewAccount() *Account {
+	return &Account{
+		Id: uuid.NewString(),
+	}
 }
 
